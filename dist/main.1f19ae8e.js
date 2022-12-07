b681fc1ced6d28e2d60e2ff40209048819d77e4d
Hyojina <hjkim3009@gmail.com>
2022-12-08 08:31:21 +0900

    Todo App-favicon

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -577,7 +577,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7828" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "2604" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
